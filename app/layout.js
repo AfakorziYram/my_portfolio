87d3a0da068d8d510aa0316538027a9f7da034f4
Yram <afakorziy@gmail.com>
2024-06-09 02:52:26 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
-import StairTransition from "@/components/stairTransition";
+import StairTransition from "@/components/StairTransition";
 
 
 // export const metadata = {
